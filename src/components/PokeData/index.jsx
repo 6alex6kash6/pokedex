fd0b94829ae28ff4ba1eafd868531177ade3b6e2
6alex6kash6 <6alex6kash6@gmail.com>
2020-12-23 17:37:30 +0300

    feat: add styles for pokedata component

--- src/components/PokeData/index.jsx
+++ src/components/PokeData/index.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import './PokeData.css'
 
 const PokeData = ({ abilities, avatar, height, id, name, type }) => {
     return (
-        <div className="card">
+        <div className='poke-data'>
             <h2>{name}</h2>
-            <div>
-                <img src={avatar} alt="pokemon" className="card-img-top" />
-            </div>
-            <div>
-                <div>№ <span>{id}</span></div>
-                <div>Type {type.map(item => {
-                    return (
-                        <div className='type' style={{ background: `var(--type-${item})` }} key={item}>{item}</div>
-                    )
-                })}</div>
-                <div>Height <span>{height}'</span></div>
-                <div>Abilities
-                    <div>
-                        {abilities.map((item, i) => {
-                            return (
-                                <p key={item}><span>{i + 1}.</span>{item}</p>
-                            )
-                        })}
+            <div className='poke-data__wrapper'>
+                <div className='poke-data__avatar'>
+                    <img src={avatar} alt="pokemon" className="card-img-top" />
+                </div>
+                <div className='poke-data__meta'>
+                    <div className='poke-data__id'><span className='poke-data__title'>№</span>  <span>{id}</span></div>
+                    <div className='poke-data__type'><span className='poke-data__title'>Type</span>  {type.map(item => {
+                        return (
+                            <div className='type' style={{ background: `var(--type-${item})`, width: '90px' }} key={item}>{item}</div>
+                        )
+                    })}</div>
+                    <div><span className='poke-data__title'>Height</span>  <span>{height}'</span></div>
+                    <div className='poke-data__abilities'>
+                        <span className='poke-data__title'>Abilities</span>
+                        <div className='poke-data__abilities--text'>
+                            {abilities.map((item, i) => {
+                                return (
+                                    <span key={item}><span>{i + 1}.</span>{item}</span>
+                                )
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
